ef6b31839687145b3230329271c894e4732f75be
arasmus8 <arasmus8@gmail.com>
2020-12-10 14:40:08 -0700

    feat: improve single card view popup when viewing upgraded cards from rewards.

---
+++ src/main/java/patches/SingleCardViewPopupFields.java
@@ -0,0 +1,17 @@
+package patches;
+
+import com.evacipated.cardcrawl.modthespire.lib.SpireField;
+import com.evacipated.cardcrawl.modthespire.lib.SpirePatch;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.screens.SingleCardViewPopup;
+
+import java.util.ArrayList;
+
+@SpirePatch(
+        clz = SingleCardViewPopup.class,
+        method = SpirePatch.CLASS
+)
+public class SingleCardViewPopupFields {
+    public static SpireField<ArrayList<AbstractCard>> unupgradedCardRewards = new SpireField<>(() -> null);
+    // public static SpireField<AbstractCard> unupgradedPopupCard = new SpireField<AbstractCard>(() -> null);
+}
